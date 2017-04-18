aafdc8b28e28610455109c616ed933c859ac9432
ISC-VANBT <ISC-VANBT>
2017-04-18 15:12:19 +0700

    update

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function getOptions(url){
                 headers: {
                     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
                 },
-                proxy: 'http://proxy.fpt.vn:80',
                 method: 'GET',
                 pool: separateReqPool
                 
